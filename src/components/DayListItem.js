f14f16c79015caaaf2c4417fdd2c31558f7a43f7
Jodie Drover <jodiedrover@live.com>
2023-01-10 18:29:47 -0500

    use classnames library to conditionally apply classes for styling

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React from 'react';
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
+  
+  const classNamesObject = classNames("day-list__item", {
+    "--selected": props.selected,
+    "--full": props.spots === 0
+  });
+
+  const dayListClass = classNamesObject.replace(" ", "");
+  console.log(dayListClass)
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayListClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
