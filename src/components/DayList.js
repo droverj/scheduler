48130ffbf293cd1cd6f80c0fc3b60059cefb77bb
Jodie Drover <jodiedrover@live.com>
2023-01-11 15:09:26 -0500

    Map over the days array from props to return DayListItem components as childnre

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,8 +2,17 @@ import React from "react";
 
 import DayListItem from './DayListItem';
 
-export default function(props) {
+export default function DayList(props) {
+  const items = props.days;
+
   return (
-    <ul></ul>
+    <ul>
+      {items.map(item => <DayListItem 
+      key={item.id}
+      name={item.name}
+      spots={item.spots}
+      selected={item.days === props.day}
+      setDay={props.setDay} />)}
+    </ul>
   );
 }
