c0d79c1a7996e9c9a21488b78145081b8af44b2f
Jodie Drover <jodiedrover@live.com>
2023-01-14 14:16:44 -0500

    Conditionally render the selected interviewer's name

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import 'components/InterviewerListItem.scss';
 
 export default function InterviewerListItem(props) {
+  const [interviewer, setInterviewer] = useState("");
+  // console.log(props);
+
+
+  function getName() {
+    if (props.id === interviewer) {
+      return props.name;
+    }
+  }
+
   return (
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
+        onClick={() => setInterviewer(props.id)}
         key={props.id}
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {getName()}
     </li>
   );
 }
