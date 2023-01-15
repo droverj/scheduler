645ea76e5b23b21579ab5d29a9efb5adaa42faae
Jodie Drover <jodiedrover@live.com>
2023-01-15 17:27:40 -0500

    Create Show component with stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -69,9 +70,7 @@ storiesOf("Button", module)
     .add("Tuesday", () => (
       <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
     ))
-    .add("Wednesday", () => (
-        <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
-    ));
+ç
 
     const interviewer = {
       id: 1,
@@ -144,3 +143,10 @@ storiesOf("Button", module)
         .add("Appointment with Time", () => <Appointment time="12pm" />)
         .add("Header", () => <Header time="12pm" />)
         .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+        .add("Show", () => (
+          <Show
+          student="Lydia Miller-Jones"
+          interviewer={interviewer}
+          onEdit={action("onEdit")}
+          onDelete={action("onDelete")} />
+        ));
