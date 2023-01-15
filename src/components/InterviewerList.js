48569e2dcdec4949e6e4a3312bb0b85d17116505
Jodie Drover <jodiedrover@live.com>
2023-01-14 21:36:23 -0500

    Build InterviewerList component to create an array of InterviewerListItems to render

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
 
+import InterviewerListItem from './InterviewerListItem';
 import 'components/InterviewerList.scss';
 
 export default function InterviewerList(props) {
+  const interviewers = props.interviewers;
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+
+      {interviewers.map(person => <InterviewerListItem
+        key={person.id}
+        name={person.name}
+        avatar={person.avatar}
+        selected={person.id === props.interviewer}
+        setInterviewer={(event) => props.setInterviewer(person.id)}
+        />)}
+        </ul>
     </section>
   );
 }
