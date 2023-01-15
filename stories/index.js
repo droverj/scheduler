2c382e226972aa3c935ed291846f677c5953a7fa
Jodie Drover <jodiedrover@live.com>
2023-01-15 17:59:33 -0500

    Create Status component and stories for it

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -158,3 +159,10 @@ storiesOf("Button", module)
         onCancel={action("onCancel")}
          />
         ))
+        .add("Status", () => <Status message="Deleting" />)
+        .add("Error", () => (
+          <Error
+          message="Could not delete appointment."
+          onClose={action("onClose")}
+           />
+          ))
