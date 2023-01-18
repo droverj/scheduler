017cd3c60d1072768f3fe861e35e747e258d1509
Jodie Drover <jodiedrover@live.com>
2023-01-18 17:22:57 -0500

    Create and export useVisualMode function for custom hook

---
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -0,0 +1,6 @@
+export function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+
+  return { mode };
+}
+
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,3 +1,15 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 
 import useVisualMode from "hooks/useVisualMode";
+
+// When the Appointment component loads, we want it to be empty
+// Initialize the mode to EMPTY. 
+// Create a mode called FIRST.
+const FIRST = "FIRST";
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
