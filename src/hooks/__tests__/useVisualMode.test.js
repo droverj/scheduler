105aa26c18e64c4b91dc54162ac2e50ac802c44f
Jodie Drover <jodiedrover@live.com>
2023-01-18 17:43:43 -0500

    Swap files and import useState

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,6 +1,22 @@
-export function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+import { renderHook, act } from "@testing-library/react-hooks";
 
-  return { mode };
-}
+import useVisualMode from "hooks/useVisualMode";
 
+// When the Appointment component loads, we want it to be empty
+// Initialize the mode to EMPTY. 
+// Create a mode called FIRST.
+const FIRST = "FIRST";
+const SECOND = "SECOND";
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
