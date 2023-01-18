239dba8fb9bcc96e075d0573eb1657de6adb677f
Jodie Drover <jodiedrover@live.com>
2023-01-18 00:24:35 -0500

    Combine the state for day, days and appointments into an object

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,52 +5,18 @@ import "components/Application.scss";
 import DayList from './DayList';
 import Appointment from '../components/Appointment'
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 export default function Application() {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));;
 
   useEffect(() => {
     axios.get("/api/days").then((response) => {
-    console.log(response.data);
     setDays([...response.data])
   })
 }, []);
@@ -66,8 +32,8 @@ export default function Application() {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
@@ -78,7 +44,7 @@ export default function Application() {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map(appointment => <Appointment
+        {Object.values(state.appointments).map(appointment => <Appointment
           key={appointment.id}
           {...appointment}
         />
