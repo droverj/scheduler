0cedee1750fd2276d275e38134db75c01c17fda8
Jodie Drover <jodiedrover@live.com>
2023-01-18 21:29:11 -0500

    Call the transition function to change the mode to CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,13 +4,13 @@ import 'components/Appointment/styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
 import { useVisualMode } from 'hooks/useVisualMode'
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -20,13 +20,14 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={(e) => e = transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       )}
+      {mode === CREATE && <Form interviewers={[]} />}
     </article>
   );
 }
