ee62fbdf655a32aac49de7ee75135803fd360927
Jodie Drover <jodiedrover@live.com>
2023-01-23 19:56:26 -0500

    Create function to update spots remaining in state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
+import { getAppointmentsForDay } from "helpers/selectors";
+
 export default function useControlledInput(initial) {
   const [state, setState] = useState({
     day: "Monday",
@@ -19,6 +21,25 @@ export default function useControlledInput(initial) {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
   }, []);
+
+
+  function updateSpots() {
+    let count = 0;
+    const result = getAppointmentsForDay(state, state.day);
+    result.forEach(id => {
+      if (id.interview === null) {
+        count += 1;
+      }
+    })
+
+    state.days.forEach(id => {
+      if (id.name === state.day) {
+        id.spots = count;
+      }
+    })
+  }
+
+  // updateSpots();
 
   function bookInterview(id, interview) {
     console.log("book interview called")
