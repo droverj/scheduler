0eb7d8638c92e831c65e9c6fffd3d4af5c0c726f
Jodie Drover <jodiedrover@live.com>
2023-01-28 16:17:31 -0500

    Add a data-testid=appointment prop to the <article> root element in the src/components/Appointments/index.js file

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, getByText, prettyDOM, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -17,6 +17,9 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    console.log(prettyDOM(container))
   })
 });
