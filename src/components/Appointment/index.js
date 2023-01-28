cf6a6475e5ffdb1d559ac6ba22e3cff6bbd6a283
Jodie Drover <jodiedrover@live.com>
2023-01-28 17:23:00 -0500

    Add 4 fireEvents for testing Appointment and include put in the axios mock

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -31,10 +31,6 @@ export default function Appointment(props) {
       interviewer
     };
 
-    // if (!interviewer || !name) {
-    //   return transition(ERROR_SAVE, true);
-    // }
-
     transition(SAVING);
 
     props
