064ef382612f9dca600ab47bbf7e0cc54a5175a9
Jodie Drover <jodiedrover@live.com>
2023-02-02 08:26:57 -0500

    test to load all of the data before we book an appointment

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -16,7 +16,10 @@ export default function useControlledInput(initial) {
       axios.get('api/appointments'),
       axios.get('api/interviewers')
     ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      setState(prev => ({ ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data }));
     })
   }, []);
 
@@ -24,40 +27,6 @@ export default function useControlledInput(initial) {
   //////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////
 
-
-  async function updateSpots(reduce = true) {
-
-    let currentDay = state.days.filter(day => {
-      if (day.name === state.day) {
-        return day;
-      }
-    })
-
-    const currentDayId = currentDay[0].id;
-
-    let spots = state.days[currentDayId].spots
-
-    if (!reduce) {
-      spots += 1;
-    } else {
-      spots -= 1;
-    }
-
-    const dayArrIndex = currentDayId - 1;
-
-    const day = {
-      ...state.days[dayArrIndex],
-      spots: spots
-    };
-    console.log(day)
-
-    const days = {
-      ...state.days,
-      [dayArrIndex]: day
-    };
-
-    return days;
-  }
 
   //////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////
@@ -75,7 +44,6 @@ export default function useControlledInput(initial) {
     try {
       const response = await axios.put(`/api/appointments/${id}`, { interview });
       setState({ ...state, appointments });
-      // updateSpots()
     } catch (error) {
       console.log('There was an error!', error);
       return error;
@@ -95,12 +63,11 @@ export default function useControlledInput(initial) {
     try {
       const response = await axios.delete(`/api/appointments/${id}`);
       setState({ ...state, appointments });
-      // updateSpots()
     } catch (error) {
       console.log('There was an error!', error);
       return error;
     }
   }
 
-  return { state, setDay, bookInterview, cancelInterview, updateSpots }
+  return { state, setDay, bookInterview, cancelInterview }
 }
