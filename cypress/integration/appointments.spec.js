cbf201930a6a6c73fc1dfaa1dcfb9652608a453a
Jodie Drover <jodiedrover@live.com>
2023-02-02 17:34:40 -0500

    Add should edit an interview test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,6 +1,6 @@
 describe("Appointments", () => {
   beforeEach(() => {
-    cy.request("GET", "/api/debug/reset");
+    // cy.request("GET", "/api/debug/reset");
 
     cy.visit("/");
 
@@ -13,11 +13,25 @@ describe("Appointments", () => {
       .click();
 
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    cy.get('[alt="Sylvia Palmer"]').click();
+    cy.get('[alt="Tori Malcolm"]').click();
 
     cy.contains("Save").click();
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get('[alt="Tori Malcolm"]').click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
