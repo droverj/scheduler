0bab4fc2064288d4fb3ddb939a43f88055a62a8c
Jodie Drover <jodiedrover@live.com>
2023-02-09 12:51:31 -0500

    Add comments to useVisualMode hook to describe functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,25 +1,39 @@
 import { useState } from "react";
 
+/**
+ *
+ * @param {*} initial
+ * @returns a custom hook for determining which mode a user should view based on current or previous state
+ */
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-
+  /**
+   *
+   * @param {*} nextMode
+   * @param {*} replace
+   * Transitions to the next mode and saves the history
+   * If replace = true, skip transitioning to the next mode and replace it with the Error mode
+   */
   function transition(nextMode, replace = false) {
     if (replace) {
-      setHistory(prev => prev.splice(2, 1, nextMode));
+      setHistory((prev) => prev.splice(2, 1, nextMode));
     } else {
-      setHistory(prev => [...prev, nextMode]);
+      setHistory((prev) => [...prev, nextMode]);
     }
     setMode(nextMode);
   }
 
+  /**
+   * Returns the user to the previous mode
+   */
   function back() {
     if (history.length <= 1) {
       setMode(initial);
     }
     if (history.length > 1) {
-      history.pop()
+      history.pop();
       const last = history.pop();
       setMode(last);
     }
