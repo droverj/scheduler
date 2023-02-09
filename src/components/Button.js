b824adabe17851915f52ace09e224eaa625fed3a
Jodie Drover <jodiedrover@live.com>
2023-02-09 12:18:54 -0500

    Add comment to describe button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,25 @@
 import React from "react";
 import classNames from "classnames";
-
 import "components/Button.scss";
 
+/**
+ *
+ * @param {*} props
+ * @returns a button component with styling to indicate to the user they can safely proceed or to proceed with caution
+ */
 export default function Button(props) {
   const buttonClass = classNames("button", {
     "button--confirm": props.confirm,
-    "button--danger": props.danger
+    "button--danger": props.danger,
   });
 
-   return (
-   <button
-   className={buttonClass}
-   onClick={props.onClick}
-   disabled={props.disabled}
-   >
-    {props.children}
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
     </button>
-   );
+  );
 }
